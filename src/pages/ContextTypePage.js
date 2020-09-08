6ba5cd8de010451c2ea913257a0ecc77513337ab
qsb3008 <qsb3008@163.com>
2020-09-08 22:55:21 +0800

    feat: context 学习

--- src/pages/ContextTypePage.js
+++ src/pages/ContextTypePage.js
@@ -1,8 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component, useContext } from 'react'
 import { ThemeContext } from '../Context'
 
+export  function Child (props) {
+    const themeContext = useContext(ThemeContext)
+    console.log(themeContext)
+    return <div className={themeContext.themeColor}>
+        uuu
+    </div>
+}
 export default class ContextTypePage extends Component {
     // 这个获取的context，会去到this.context
+    // 这里的contextType是api来的，https://zh-hans.reactjs.org/docs/context.html#classcontexttype
+    // 记住这么用就好
+    // 或者这么用：
+    // class xxx extends Component {}
+    // xxx.contextType = ThemeContext
+    // export default xxx
+    // 如果在函数组件要用 useContext
     static contextType = ThemeContext
     render() {
         const { themeColor } = this.context
@@ -11,6 +25,7 @@ export default class ContextTypePage extends Component {
         return (
             <div className={themeColor}>
                 type
+                <Child></Child>
             </div>
         )
     }
