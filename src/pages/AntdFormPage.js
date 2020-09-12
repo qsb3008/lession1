5bad87b785df47fc2d6f2c8e7037eb59c147fbfa
qsb3008 <qsb3008@163.com>
2020-09-12 21:52:18 +0800

    class

--- src/pages/AntdFormPage.js
+++ src/pages/AntdFormPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { Component } from "react";
 import { Form, Input, Button } from "antd";
 
 const FormItem = Form.Item;
@@ -6,35 +6,57 @@ const FormItem = Form.Item;
 const nameRules = { required: true, message: "请输入姓名！" };
 const passworRules = { required: true, message: "请输入密码！" };
 
-export default function AntdFormPage(props) {
-    const [form] = Form.useForm();
-    // 表单校验成功执行
-    const onFinish = val => {
+export default class AntdFormPage extends Component {
+    // formRef = xxx 这样写，后面可以通过this.formRef使用
+    // 需要动态改变的才写在state里面
+    formRef = React.createRef();
+    componentDidMount() {
+        this.formRef.current.setFieldsValue({
+            name: "default"
+        });
+    }
+    onReset = () => {
+        this.formRef.current.resetFields();
+    };
+    onFinish = val => {
         console.log("onFinish", val); //sy-log
     };
-    // 表单校验失败执行
-    const onFinishFailed = val => {
+    onFinishFailed = val => {
         console.log("onFinishFailed", val); //sy-log
     };
-
-    useEffect(() => {
-        console.log("form", form); //sy-log
-        form.setFieldsValue({ name: "default" });
-    }, [form]);
-
-    return (
-        <Form form={form} onFinish={onFinish} onFinishFailed={onFinishFailed}>
-            <FormItem label="姓名" name="name" rules={[nameRules]}>
-                <Input placeholder="name input placeholder" />
-            </FormItem>
-            <FormItem label="密码" name="password" rules={[passworRules]}>
-                <Input placeholder="password input placeholder" />
-            </FormItem>
-            <FormItem>
-                <Button type="primary" size="large" htmlType="submit">
-                    Submit
-                </Button>
-            </FormItem>
-        </Form>
-    );
+    render() {
+        console.log("AntdFormPage render",
+            this.formRef.current); //sy-log
+        return (
+            <div>
+                <h3>AntdFormPage</h3>
+                <Form
+                    ref={this.formRef}
+                    onFinish={this.onFinish}
+                    onFinishFailed={this.onFinishFailed}
+                    onReset={this.onReset}>
+                    <FormItem label="姓名" name="name" rules=
+                        {[nameRules]}>
+                        <Input placeholder="name input placeholder" />
+                    </FormItem>
+                    <FormItem label="密码" name="password"
+                        rules={[passworRules]}>
+                        <Input placeholder="password input placeholder" />
+                    </FormItem>
+                    <FormItem>
+                        <Button type="primary" size="large"
+                            htmlType="submit">
+                            Submit
+                        </Button>
+                    </FormItem>
+                    <FormItem>
+                        <Button type="default" size="large"
+                            htmlType="reset">
+                            Reset
+                        </Button>
+                    </FormItem>
+                </Form>
+            </div>
+        );
+    }
 }
