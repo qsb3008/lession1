13c429e61ffea82ce65e987c71b1c43cd7a36e8a
qsb3008 <qsb3008@163.com>
2020-09-12 22:18:37 +0800

    ref learn

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './App.css';
 // import ContextPage from './pages/ContextPage'
-import AntdFormPage from './pages/AntdFormPage'
+// import AntdFormPage from './pages/AntdFormPage'
+import RefLearn from './pages/RefLearn'
 import 'antd/dist/antd.css';
 
+const storeRef = React.createRef()
+
 function App() {
+  const handleClick = () => {
+    console.log(storeRef)
+    storeRef.current.focus()
+  }
   return (
     <div className="App">
-      <AntdFormPage></AntdFormPage>
+      <div onClick={handleClick}>click</div>
+      <RefLearn ref={storeRef}></RefLearn>
     </div>
   );
 }
